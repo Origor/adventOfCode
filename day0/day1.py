af7ad3085b3556fbba2b2986b80e65aeb1bdf5ab
Igor Pietrzak <ajgorpietrzak@gmail.com>
2021-12-03 23:15:53 +0100

    Add Rust solution for day 1

--- day0/day1.py
+++ day0/day1.py
@@ -8,12 +8,28 @@ def part_a(data):
 def part_b(data):
     return solve(data, 3)
 
+def alternative_part_a():
+    with open("example_input.txt", "r", encoding="utf-8") as f:
+        lines = f.readlines()
+
+    c = 0
+    for i in range(len(lines)):
+        if i == 0:
+            print(lines[i][:-1], "(N/A)", c)
+        elif lines[i - 1] < lines[i]:
+            c += 1
+            print(lines[i][:-1], "(increased)", c)
+        else:
+            print(lines[i][:-1], "(decreased)", c)
+    return c
+
 def main():
-    with open("part1_input.txt", "r", encoding="utf-8") as inputFile:
+    with open("input.txt", "r", encoding="utf-8") as inputFile:
         data = list(map(int, inputFile.readlines()))
 
     print(part_a(data))
     print(part_b(data))
+    print(alternative_part_a())
 
 if __name__ == "__main__":
     main()
--- day0/main.rs
+++ day0/main.rs
@@ -1,21 +1,68 @@
-use std::env;
+//use std::env;
 use std::fs;
 
-fn main() {
-    let filename = "example_input.txt";
-    println!("Hello, world!");
-    println!("In file {}", filename);
+fn part_a(input: &String) -> i32 {
+    let vector: Vec<i32> = parse_input(input);
 
-    let contents: i32 = fs::read_to_string(filename)
-        .expect("Something went wrong reading the file");
+    let mut increased_counter = 0;
+    for (index, item) in vector.iter().enumerate() {
+        //println!("Element at position {}: {:?}", index, item);
+        if index == 0 {
+
+        } else if item > &vector[index - 1] {
+            increased_counter += 1;
+        }
+    }
+    return increased_counter;
+}
+
+fn part_b(input: &String) -> i32 {
+    let vector: Vec<i32> = parse_input(input);
+
+    let mut increased_counter = 0;
+    for (index, item) in vector.iter().enumerate() {
+        if index < 3 {} 
+        else if item > &vector[index - 3] {
+            increased_counter += 1;
+        }
+    }
+    return increased_counter;
+}
+
+fn part_c(input: &String, window: usize) -> i32 {
+    let vector: Vec<i32> = parse_input(input);
+
+    let mut increased_counter = 0;
+    for (index, item) in vector.iter().enumerate() {
+        if index < window {} 
+        else if item > &vector[index - window] {
+            increased_counter += 1;
+        }
+    }
+    return increased_counter;
+}
+
+fn parse_input(input: &String) -> Vec<i32> {
+    let mut vector: Vec<i32> = Vec::new();
     
-    
-    for text in contents {
-        text.parse::<i32>().unwrap();
+    for item in input.split_whitespace() {
+        vector.push(item.parse::<i32>().unwrap());
     }
 
+    return vector;
+}
 
-    contents.collect::<Vec<i32>>();
+fn main() {
+    let filename = "input.txt";
+    println!("In file {}", filename);
 
-    println!("With text:\n{}", contents);
+    let contents: String = fs::read_to_string(filename)
+        .expect("Something went wrong reading the file");
+    
+    //println!("Our current string: {:?}", contents);
+
+    println!("Part 1: {:?}", part_a(&contents));
+    println!("Part 2: {:?}", part_b(&contents));
+    println!("Part 1 with window: {:?}", part_c(&contents, 1));
+    println!("Part 2 with window: {:?}", part_c(&contents, 3));
 }
